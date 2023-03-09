10e02a891c6fa87fb8cffd38a4bb10cc02ec64a5
Elkhen <elkhen@gmail.com>
2023-03-09 23:13:25 +0100

    Refactor of Client tests.

--- src/main/java/org/mati/model/Recipe.java
+++ src/main/java/org/mati/model/Recipe.java
@@ -5,14 +5,25 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
 @Entity
+@NoArgsConstructor
 public class Recipe {
+
+    public Recipe(String name, String category, String description, @NotNull @Size(min = 1) String[] ingredients, @NotNull @Size(min = 1) String[] directions) {
+        this.name = name;
+        this.category = category;
+        this.description = description;
+        this.ingredients = ingredients;
+        this.directions = directions;
+    }
 
     @Id
     @GeneratedValue(generator = "recipe_generator")
--- src/test/java/org/mati/test/RecipeRestClientTest.java
+++ src/test/java/org/mati/test/RecipeRestClientTest.java
@@ -8,7 +8,6 @@ import jakarta.ws.rs.client.Entity;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import org.apache.log4j.Logger;
-import org.junit.Assert;
 import org.junit.Test;
 import org.mati.model.Recipe;
 
@@ -20,87 +19,115 @@ public class RecipeRestClientTest
     private static final Logger logger = Logger.getLogger(RecipeRestClientTest.class);
     private static final String REST_TARGET_URL = "http://localhost:8080/recipes/api/recipe";
 
+    final Recipe[] RECIPES = {
+        new Recipe(
+                    "Fresh Mint Tea",
+                    "beverage",
+                    "Light, aromatic and refreshing beverage, ... ",
+                    new String[]{"boiled water", "honey", "fresh mint leaves"},
+                    new String[]{"Boil water",
+                            "Pour boiling hot water into a mug",
+                            "Add fresh mint leaves",
+                            "Mix and let the mint leaves seep for 3-5 minutes",
+                            "Add honey and mix again"}
+        ),
+        new Recipe(
+                    "Warming Ginger Tea",
+                    "beverage",
+                    "Ginger tea is a warming drink for cool weather, ...",
+                    new String[]{"1 inch ginger root, minced", "1/2 lemon, juiced", "1/2 teaspoon manuka honey"},
+                    new String[]{"Place all ingredients in a mug and fill with warm water " +
+                            "(not too hot so you keep the beneficial honey compounds in tact)",
+                            "Steep for 5-10 minutes", "Drink and enjoy"}
+        )
+    };
+
+    final Recipe[] INCORRECT_RECIPES = {
+            //0
+            new Recipe(
+                    null,
+                    "beverage",
+                    "Light, aromatic and refreshing beverage, ...",
+                    new String[]{"boiled water", "honey", "fresh mint leaves"},
+                    new String[]{"Boil water", "Pour boiling hot water into a mug",
+                            "Add fresh mint leaves",
+                            "Mix and let the mint leaves seep for 3-5 minutes",
+                            "Add honey and mix again"}
+            ),
+            //1
+            new Recipe(
+                    "Fresh Mint Tea",
+                    null,
+                    "Light, aromatic and refreshing beverage, ...",
+                    new String[]{"boiled water", "honey", "fresh mint leaves"},
+                    new String[]{"Boil water", "Pour boiling hot water into a mug",
+                            "Add fresh mint leaves",
+                            "Mix and let the mint leaves seep for 3-5 minutes",
+                            "Add honey and mix again"}
+            )
+    };
+
     @Test
     public void crudTests() {
-        Recipe recipe = new Recipe();
-        recipe.setName("Warming Ginger Tea");
-        recipe.setCategory("beverage");
-        recipe.setDescription("Ginger tea is a warming drink for cool weather, ...");
-        recipe.setIngredients(new String[]{"1 inch ginger root, minced", "1/2 lemon, juiced", "1/2 teaspoon manuka honey"});
-        recipe.setDirections(new String[]{"Boil water", "Pour boiling hot water into a mug", "Add fresh mint leaves",
-                "Mix and let the mint leaves seep for 3-5 minutes",
-                "Add honey and mix again"});
+        logger.info("Adding the recipe.");
 
-        Recipe incorrectRecipe = new Recipe();
-        incorrectRecipe.setName(null);
-        recipe.setCategory("beverage");
-        incorrectRecipe.setDescription("Ginger tea is a warming drink for cool weather, ...");
-        incorrectRecipe.setIngredients(new String[]{"1 inch ginger root, minced", "1/2 lemon, juiced", "1/2 teaspoon manuka honey"});
-        incorrectRecipe.setDirections(new String[]{"Boil water", "Pour boiling hot water into a mug", "Add fresh mint leaves",
-                "Mix and let the mint leaves seep for 3-5 minutes",
-                "Add honey and mix again"});
+        Response correctRecipeResponse = addTest(RECIPES[0]);
+        assertEquals(200, correctRecipeResponse.getStatus());
+
+        Response incorrectRecipeResponse = addTest(INCORRECT_RECIPES[0]);
+        assertEquals(400, incorrectRecipeResponse.getStatus());
+
+        int idOfPostedRecipe = (int) correctRecipeResponse.readEntity(Map.class).get("id");
 
 
-        logger.info("POSTing the correct recipe.");
+        logger.info("Getting the recipe.");
 
-        Response correctResponse = ClientBuilder.newClient()
+        Recipe fetchedRecipe = getTest(idOfPostedRecipe);
+        assertEquals(RECIPES[0], fetchedRecipe);
+
+
+        logger.info("Updating the recipe.");
+
+        RECIPES[0].setName("NotSoFresh Mint Tea");
+        Response updateResponse =  updateTest(RECIPES[0], idOfPostedRecipe);
+        assertEquals(204, updateResponse.getStatus());
+
+        Recipe fetchedRecipeAfterUpdate = getTest(idOfPostedRecipe);
+        assertEquals(RECIPES[0], fetchedRecipeAfterUpdate);
+
+
+        logger.info("Deleting the recipe.");
+
+        Response deleteResponse = deleteTest(idOfPostedRecipe);
+        assertEquals(204, deleteResponse.getStatus());
+
+        Response deleteNotFoundResponse = deleteTest(idOfPostedRecipe);
+        assertEquals(404, deleteNotFoundResponse.getStatus());
+
+    }
+
+    public Response addTest(Recipe recipe) {
+        return ClientBuilder.newClient()
                 .target(REST_TARGET_URL).path("/new")
                 .request().post(Entity.entity(recipe, MediaType.APPLICATION_JSON));
 
-        Assert.assertEquals(200, correctResponse.getStatus());
+    }
 
-
-        logger.info("POSTing the incorrect recipe.");
-
-        Response incorrectResponse = ClientBuilder.newClient()
-                .target(REST_TARGET_URL).path("/new")
-                .request().post(Entity.entity(incorrectRecipe, MediaType.APPLICATION_JSON));
-
-        Assert.assertEquals(400, incorrectResponse.getStatus());
-
-        int idOfPostedRecipe = (int) correctResponse.readEntity(Map.class).get("id");
-
-        logger.info("GETting the recipe");
-
-        Recipe fetchedRecipe = ClientBuilder.newClient()
-                .target(REST_TARGET_URL).path("/{id}").resolveTemplate("id", idOfPostedRecipe)
+    public Recipe getTest(long id) {
+        return ClientBuilder.newClient()
+                .target(REST_TARGET_URL).path("/{id}").resolveTemplate("id", id)
                 .request().get(Recipe.class);
+    }
 
-        assertEquals(recipe, fetchedRecipe);
+    public Response updateTest(Recipe recipe, long id) {
+        return ClientBuilder.newClient()
+                .target(REST_TARGET_URL).path("/{id}").resolveTemplate("id", id)
+                .request().put(Entity.entity(recipe, MediaType.APPLICATION_JSON));
+    }
 
-        logger.info("PUTting the recipe for update");
-
-        recipe.setName("NotSoWarming Ginger Tea");
-
-        Response updateResponse = ClientBuilder.newClient()
-                        .target(REST_TARGET_URL).path("/{id}").resolveTemplate("id", idOfPostedRecipe)
-                        .request().put(Entity.entity(recipe, MediaType.APPLICATION_JSON));
-
-        assertEquals(204, updateResponse.getStatus());
-
-        Recipe fetchedRecipeAfterUpdate = ClientBuilder.newClient()
-                .target(REST_TARGET_URL).path("/{id}").resolveTemplate("id", idOfPostedRecipe)
-                .request().get(Recipe.class);
-
-        assertEquals(recipe, fetchedRecipeAfterUpdate);
-
-
-        logger.info("DELETing existing recipe.");
-
-        Response deleteResponse = ClientBuilder.newClient()
-                .target(REST_TARGET_URL).path("/{id}").resolveTemplate("id", idOfPostedRecipe)
+    public Response deleteTest(long id) {
+        return ClientBuilder.newClient()
+                .target(REST_TARGET_URL).path("/{id}").resolveTemplate("id", id)
                 .request().delete();
-
-        assertEquals(204, deleteResponse.getStatus());
-
-
-        logger.info("Trying to DELETE a recipe that does not exist");
-
-        deleteResponse = ClientBuilder.newClient()
-                .target(REST_TARGET_URL).path("/{id}").resolveTemplate("id", idOfPostedRecipe)
-                .request().delete();
-
-        assertEquals(404, deleteResponse.getStatus());
-
     }
 }
